cf174114107948feadda166f83f3cacd5fbb68b5
Ben Walker <benjaminjwalker2910@gmail.com>
2023-06-12 15:37:53 +0100

    updating for herkou to run it happily

--- rest-api/src/main/java/com/acme/Main.java
+++ rest-api/src/main/java/com/acme/Main.java
@@ -2,12 +2,27 @@ package com.acme;
 
 import static spark.Spark.get;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Main 
 {
     public static void main( String[] args )
     {
+        /*Initialising Stuff*/
+        Dotenv dotenv = Dotenv.configure().load();
+        getHerokuAssignedPort(dotenv);
+
+        /*Setting the routes*/
         get("/items", (request, response) -> {
             return "The item is an apple.";
         });
+    }
+
+    static int getHerokuAssignedPort(Dotenv dotenv) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return Integer.parseInt(dotenv.get("DEFAULT_PORT", "4567"));  
     }
 }
