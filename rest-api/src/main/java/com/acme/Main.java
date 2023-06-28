70230729e97c9c9654d35923e915958ca3301f12
Michael <Michael O'Hanlon>
2023-06-28 14:34:54 +0100

    Got to a point with creditcard endpoing where It nearly formats the JSON correctly

--- rest-api/src/main/java/com/acme/Main.java
+++ rest-api/src/main/java/com/acme/Main.java
@@ -4,6 +4,9 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.post;
 
+import com.acme.dataobjects.CreditCard;
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -16,14 +19,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-
 import javax.swing.text.html.HTMLDocument.Iterator;
 import javax.xml.namespace.QName;
 
 import com.google.gson.JsonArray;
 
-import com.google.gson.Gson;
+
 public class Main 
 {
     public static void main( String[] args )
@@ -44,17 +45,30 @@ public class Main
         });
 
         post("/creditCard", (request, response) -> {
-            Gson g = new Gson();  
-            g.toJson(request.body()); 
-             
-            // String attempt = g.get("property_name").toString();
+            // Gson g = new Gson();  
+            // g.toJson(request.body()); 
 
+            String defaultRequestsBody = request.body();
+          
+            String requestBody = defaultRequestsBody.replaceAll("\"" , "").replaceAll("\\{" , "").replaceAll("\\}" , "");
+            String[] allParams = requestBody.split(",");
+
+            HashMap<String,String> itemList = new HashMap<String,String>();
+        
+            for (String pair : allParams) {
+                String[] keyValue = pair.split(":");
+                itemList.put(keyValue[0],keyValue[1]);
+            }
+               
+            // CreditCard usersCard = new CreditCard();
             
 
-        
-
             // return request.body().get("postcode").toString();
-            return request.body();
+            // return request.body();
+
+            response.type("application/json");
+            System.out.println(itemList.get("amount"));
+            return new Gson().toJsonTree(itemList);
         });
     }
 
