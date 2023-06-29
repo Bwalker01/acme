98bd76d9c75779edcaaab1f4ef1516e6204d83fc
Ben Walker <benjaminjwalker2910@gmail.com>
2023-06-29 11:32:40 +0100

    unit test coverage

--- rest-api/src/test/java/com/acme/database/ProductDAOTest.java
+++ rest-api/src/test/java/com/acme/database/ProductDAOTest.java
@@ -1,6 +1,7 @@
 package com.acme.database;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -8,6 +9,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.acme.dataobjects.Product;
+import com.acme.exceptions.InvalidProductException;
 
 public class ProductDAOTest {
     private static ProductDAO dao;
@@ -22,12 +24,19 @@ public class ProductDAOTest {
     }
 
     @Test
-    public void fetchItemStandard() {
+    public void fetchItem_Standard() {
         String[] mockedResult = {"123456789055", "name", "1"};
         when(mockController.getItem("123456789055")).thenReturn(mockedResult);
         Product result = dao.fetchItem("123456789055");
         assertThat(result.getBarcode()).isEqualTo(mockedResult[0]);
         assertThat(result.getName()).isEqualTo(mockedResult[1]);
         assertThat(result.getPrice()).isEqualTo(1.0);
+    }
+
+    @Test
+    public void fetchItem_InvalidBarcode() {
+        String[] mockedResult = {"1234", "name", "1"};
+        when (mockController.getItem("1234")).thenReturn(mockedResult);
+        assertThatThrownBy(() -> dao.fetchItem("1234")).isInstanceOf(InvalidProductException.class);
     }
 }
