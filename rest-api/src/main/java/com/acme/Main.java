fc0a254a614b564b614e320af9ecd76d8f21f499
Michael <Michael O'Hanlon>
2023-06-29 15:15:31 +0100

    working credit card endpoint

--- rest-api/src/main/java/com/acme/Main.java
+++ rest-api/src/main/java/com/acme/Main.java
@@ -8,23 +8,35 @@ import com.acme.dataobjects.CreditCard;
 import com.acme.dataobjects.Product;
 import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+// import java.io.BufferedReader;
+// import java.io.DataOutputStream;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.io.UnsupportedEncodingException;
+// import java.net.HttpURLConnection;
+// import java.net.URL;
+// import java.net.URLEncoder;
+// import java.net.http.HttpClient;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
 
-import javax.servlet.http.Part;
-import javax.swing.text.html.HTMLDocument.Iterator;
-import javax.xml.namespace.QName;
+// import javax.servlet.http.Part;
+// import javax.swing.text.html.HTMLDocument.Iterator;
+// import javax.xml.namespace.QName;
 
-import com.google.gson.JsonArray;
+// import com.google.gson.JsonArray;
+
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+
 
 
 public class Main 
@@ -62,14 +74,26 @@ public class Main
 
             CreditCard usersCard = new Gson().fromJson(request.body(), CreditCard.class);
 
-            System.out.println(usersCard.getAmount());
-            System.out.println(usersCard.getCreditCardNumber());
-            System.out.println(usersCard.getExpiryDate());
-            System.out.println(usersCard.getCvc());
-            System.out.println(usersCard.getAddress());
-            System.out.println(usersCard.getPostcode());
-            System.out.println(usersCard.getAccountHolderName());
-            return new Gson().toJsonTree(usersCard);
+            String postUrl  = "https://acme2pos.azurewebsites.net/payments";// put in your url
+            Gson gson = new Gson();
+
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(postUrl);
+            StringEntity postingString = new StringEntity(gson.toJson(usersCard));//gson.tojson() converts the creditCard object back to a to json
+            post.setEntity(postingString);
+            post.setHeader("Content-type", "application/json");
+
+            String authKey = "";//auth key removed for safety need to find away to make more secure
+            post.setHeader("x-authkey", authKey);
+
+            HttpResponse  responses = httpClient.execute(post);
+            HttpEntity entity = responses.getEntity();
+            String responseString = EntityUtils.toString(entity, "UTF-8");
+            System.out.println(responseString);
+
+
+            return responseString;
+
         });
     }
 
