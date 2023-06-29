709434bc243cc9cc7352023dd2e8523cb939e667
Michael <Michael O'Hanlon>
2023-06-29 15:30:21 +0100

    removed uneeded imports

--- rest-api/src/main/java/com/acme/Main.java
+++ rest-api/src/main/java/com/acme/Main.java
@@ -7,27 +7,6 @@ import static spark.Spark.post;
 import com.acme.dataobjects.CreditCard;
 import com.acme.dataobjects.Product;
 import com.google.gson.Gson;
-
-// import java.io.BufferedReader;
-// import java.io.DataOutputStream;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
-// import java.io.UnsupportedEncodingException;
-// import java.net.HttpURLConnection;
-// import java.net.URL;
-// import java.net.URLEncoder;
-// import java.net.http.HttpClient;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-
-// import javax.servlet.http.Part;
-// import javax.swing.text.html.HTMLDocument.Iterator;
-// import javax.xml.namespace.QName;
-
-// import com.google.gson.JsonArray;
-
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -91,7 +70,7 @@ public class Main
             String responseString = EntityUtils.toString(entity, "UTF-8");
             System.out.println(responseString);
 
-
+            
             return responseString;
 
         });
