2020c0739dbf1263e5ff235a4de0ec17c30abdf6
Michael <Michael O'Hanlon>
2023-07-13 11:45:55 +0100

    created Barcode class and started work on barcode endpoint

--- rest-api/src/main/java/com/acme/Main.java
+++ rest-api/src/main/java/com/acme/Main.java
@@ -4,6 +4,9 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.post;
 
+import java.util.ArrayList;
+
+import com.acme.dataobjects.Barcodes;
 import com.acme.dataobjects.CreditCard;
 import com.acme.dataobjects.Product;
 import com.google.gson.Gson;
@@ -14,11 +17,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import com.acme.database.ProductDAO;
 
 
 
 
-public class Main 
+public class Main
 {
     public static void main( String[] args )
     {
@@ -26,6 +30,10 @@ public class Main
         port(getHerokuAssignedPort());
 
         /*Setting the routes*/
+
+        ArrayList<Product> listOfItems = new ArrayList<Product>(); 
+
+
         get("/products", (request, response) -> {
             return "The product is an apple.";
         });
@@ -37,45 +45,48 @@ public class Main
             return "The item is: " + request.params(":name");
         });
 
-        // post("/barcode", (request, response) -> {
-        //     String barcode = "123456789055";
-        //     Product test = fetchItem();
-        //     response.body(barcode);
-            
-            
-            
-        //     return response.body();
-
-        // });
-
-        post("/creditCard", (request, response) -> {
-            response.type("application/json");
+        post("/barcode", (request, response) -> {
             Gson gson = new Gson();
+            ProductDAO productDatabase = new ProductDAO();
 
-            CreditCard usersCard =  gson.fromJson(request.body(), CreditCard.class);
+            Barcodes barcode = gson.fromJson(request.body(), Barcodes.class);
 
-            String postUrl = "https://acme2pos.azurewebsites.net/payments";// put in your url
-            
-            HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost post = new HttpPost(postUrl);
-            StringEntity postingString = new StringEntity(gson.toJson(usersCard));//gson.tojson() converts the creditCard object back to a to json
-            post.setEntity(postingString);
-            post.setHeader("Content-type", "application/json");
+            Product test = productDatabase.fetchItem(barcode.getBarcode());
 
-            String authKey = System.getenv("XAUTH_KEY");
-            System.out.println(authKey);//auth key removed for safety need to find away to make more secure
-            post.setHeader("x-authkey", authKey);
-
-            HttpResponse responses = httpClient.execute(post);
-            HttpEntity entity = responses.getEntity();
-            String responseString = EntityUtils.toString(entity, "UTF-8");
-            System.out.println(responseString);
-
-
-            return responseString;
+            listOfItems.add(test);
+            return listOfItems;
 
         });
     }
+
+    //     post("/creditCard", (request, response) -> {
+    //         response.type("application/json");
+    //         Gson gson = new Gson();
+
+    //         CreditCard usersCard =  gson.fromJson(request.body(), CreditCard.class);
+
+    //         String postUrl = "https://acme2pos.azurewebsites.net/payments";// put in your url
+            
+    //         HttpClient httpClient = HttpClientBuilder.create().build();
+    //         HttpPost post = new HttpPost(postUrl);
+    //         StringEntity postingString = new StringEntity(gson.toJson(usersCard));//gson.tojson() converts the creditCard object back to a to json
+    //         post.setEntity(postingString);
+    //         post.setHeader("Content-type", "application/json");
+
+    //         String authKey = System.getenv("XAUTH_KEY");
+    //         System.out.println(authKey);//auth key removed for safety need to find away to make more secure
+    //         post.setHeader("x-authkey", authKey);
+
+    //         HttpResponse responses = httpClient.execute(post);
+    //         HttpEntity entity = responses.getEntity();
+    //         String responseString = EntityUtils.toString(entity, "UTF-8");
+    //         System.out.println(responseString);
+
+
+    //         return responseString;
+
+    //     });
+    // }
 
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
