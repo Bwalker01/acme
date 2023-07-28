c16a3d2ca601a431b36207f405a64b8f05fe96fc
MattyHoughton <mattyh1997@hotmail.com>
2023-07-28 16:08:24 +0100

    Up to date JS & HTML files

--- web-app/webapp.js
+++ web-app/webapp.js
@@ -1,13 +1,14 @@
+// import axios, { isCancel, AxiosError } from 'axios';
+
 let recievedJSON;
 let itemsJSON;
 
 function submitBarcode() {
     let barcode = document.getElementById('barcode').value;
-    let barcodeJSON =
-    {
-        "barcode": barcode
-    };
+    let barcodeJSON = `{ "barcode": ${barcode} }`;
 
+    console.log(barcode);
+    console.log(barcodeJSON);
     recievedJSON = getItems(barcodeJSON);
     itemsJSON = recievedJSON.items;
     let totalPrice = recievedJSON.total;
@@ -22,14 +23,25 @@ function submitBarcode() {
 }
 
 async function getItems(barcode) {
+    
     const URL = "https://project-acme.herokuapp.com/barcode";
 
     try {
         let response = await fetch(URL, {
             method: 'POST',
-            body: barcode
-        })
 
+            headers: {
+                'Accept': 'application/json',
+                "Content-Type": "application/json",
+                'Access-Control-Request-Origin': 'http://localhost:8080/',
+                'Access-Control-Request-Method': 'POST',
+                
+            },
+            body: JSON.stringify({
+                "barcode":123456789055
+            })
+        })
+        console.log(response);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -39,7 +51,45 @@ async function getItems(barcode) {
     } catch (error) {
         console.error(error);
     }
+    
+    /*
+const URL = "https://project-acme.herokuapp.com/barcode";
+const barcodeEntered = {
+    barcode:123456789055
 }
+axios({
+    method: 'post',
+    url: URL,
+    data: {
+        barcodeEntered
+    }
+})
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+*/
+    /*
+    fetch("https://project-acme.herokuapp.com/barcode", {
+        method: 'POST',
+        body: barcode,
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+        },
+        mode: "no-cors",
+    })
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            
+        }).catch(error => console.error('Error:', error));
+        */
+
+};
+
+
 
 function buildTable(data) {
     let table = document.getElementById('itemTable');
@@ -113,6 +163,7 @@ async function submitPayment() {
     try {
         let response = await fetch(URL2, {
             method: 'POST',
+            mode: "no-cors",
             body: paymentJSON
         })
 
