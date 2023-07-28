5ec854f9224b27171ad528be3a6e47b095f9629d
Ben Walker <benjaminjwalker2910@gmail.com>
2023-07-28 13:06:29 +0100

    status and header inclusion

--- rest-api/src/main/java/com/acme/Main.java
+++ rest-api/src/main/java/com/acme/Main.java
@@ -1,4 +1,5 @@
 package com.acme;
+
 import static spark.Spark.port;
 import static spark.Spark.post;
 import static spark.Spark.delete;
@@ -13,8 +14,6 @@ import com.acme.dataobjects.OutputProduct;
 import com.acme.dataobjects.Product;
 import com.google.gson.Gson;
 
-
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -24,89 +23,84 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import com.acme.database.ProductDAO;
 
-
-
-
-
-public class Main
-{
+public class Main {
     public static double totalPrice;
     public static int quantity;
-    public static void main( String[] args )
-    {
-        /*Initialising Listening Port*/
+
+    public static void main(String[] args) {
+        /* Initialising Listening Port */
         port(getHerokuAssignedPort());
-        
+
         Gson gson = new Gson();
 
         HashMap<Product, Integer> items = new HashMap<>();
         ProductDAO productDatabase = new ProductDAO();
-        HashMap<DiscountBundle, Integer> discountBundleMap = new HashMap<>();
-        
-    
+        // HashMap<DiscountBundle, Integer> discountBundleMap = new HashMap<>();
 
-        /*Setting the routes*/
+        /* Setting the routes */
         post("/barcode", (request, response) -> {
             String barcode = gson.fromJson(request.body(), Barcodes.class).getBarcode();
-            System.out.println(barcode);
-            if(barcode.equals("END")) {
+            if (barcode.equals("END")) {
                 System.out.println("end");
                 // for (Product key : items.keySet()) {
-                //     DiscountBundle discountBundle = productDatabase.checkForBundle(key);
-                    
+                // DiscountBundle discountBundle = productDatabase.checkForBundle(key);
 
-                //     if(!discountBundle.equals(null)){
-                //         int amount = discountBundle.getQuantity();
+                // if(!discountBundle.equals(null)){
+                // int amount = discountBundle.getQuantity();
 
-                //         int calcAmount = amount % quantity;
+                // int calcAmount = amount % quantity;
 
-                //         int finalAmount = (quantity - calcAmount) / amount; 
+                // int finalAmount = (quantity - calcAmount) / amount;
 
-                //         discountBundleMap.put(discountBundle, finalAmount);
-                //     }
-                    
-                    
+                // discountBundleMap.put(discountBundle, finalAmount);
+                // }
 
-                //     ItemResponse responses = new ItemResponse(items);
-                        
-                        
-                //     System.out.println(gson.toJsonTree(responses));
-                
-                //}
+                // ItemResponse responses = new ItemResponse(items);
+
+                // System.out.println(gson.toJsonTree(responses));
+
+                // }
             } else {
                 for (Product key : items.keySet()) {
-                    if(key.getBarcode().equals(barcode)) {
-                        items.replace(key, items.get(key)+1);
+                    if (key.getBarcode().equals(barcode)) {
+                        items.replace(key, items.get(key) + 1);
                         ItemResponse finalResponse = new ItemResponse(items);
+                        response.status(200);
+                        response.header("Content-Type", "application/json");
+                        response.header("Access-Control-Allow-Origin", "*");
                         return gson.toJsonTree(finalResponse);
                     }
                 }
                 items.put(productDatabase.fetchItem(barcode), 1);
                 ItemResponse finalResponse = new ItemResponse(items);
+                response.status(200);
+                response.header("Content-Type", "application/json");
+                response.header("Access-Control-Allow-Origin", "*");
                 return gson.toJsonTree(finalResponse);
             }
             ItemResponse finalResponse = new ItemResponse(items);
-            return gson.toJsonTree(finalResponse);
+            response.body(gson.toJson(finalResponse));
+            response.status(200);
+            response.header("Content-Type", "application/json");
+            return response;
         });
 
         delete("/barcode", (request, response) -> {
             String barcode = gson.fromJson(request.body(), Barcodes.class).getBarcode();
-            if(items.size()>=1){
-
-                for(Product product: items.keySet()){
-                    if(product.getBarcode().equals(barcode)){
-                        if(items.get(product) == 1){
+            if (items.size() >= 1) {
+                for (Product product : items.keySet()) {
+                    if (product.getBarcode().equals(barcode)) {
+                        if (items.get(product) == 1) {
                             items.remove(product);
-                        }else{
-                            items.replace(product, items.get(product)-1);
+                        } else {
+                            items.replace(product, items.get(product) - 1);
                         }
 
                     }
                 }
-                    
             }
-                ItemResponse finalResponse = new ItemResponse(items);
-                return gson.toJsonTree(finalResponse);
+            ItemResponse finalResponse = new ItemResponse(items);
+            return gson.toJsonTree(finalResponse);
         });
 
         post("/creditCard", (request, response) -> {
@@ -115,15 +109,16 @@ public class Main
             CreditCard usersCard = gson.fromJson(request.body(), CreditCard.class);
 
             String postUrl = "https://acme2pos.azurewebsites.net/payments";// put in your url
-            
+
             HttpClient httpClient = HttpClientBuilder.create().build();
             HttpPost post = new HttpPost(postUrl);
-            StringEntity postingString = new StringEntity(gson.toJson(usersCard));//gson.tojson() converts the creditCard object back to a to json
+            StringEntity postingString = new StringEntity(gson.toJson(usersCard));// gson.tojson() converts the
+                                                                                  // creditCard object back to a to json
             post.setEntity(postingString);
             post.setHeader("Content-type", "application/json");
 
             String authKey = System.getenv("XAUTH_KEY");
-            System.out.println(authKey);//auth key removed for safety need to find away to make more secure
+            System.out.println(authKey);// auth key removed for safety need to find away to make more secure
             post.setHeader("x-authkey", authKey);
 
             HttpResponse responses = httpClient.execute(post);
@@ -131,9 +126,7 @@ public class Main
             String responseString = EntityUtils.toString(entity, "UTF-8");
             System.out.println(responseString);
 
-
             return responseString;
-
         });
     }
 
@@ -141,21 +134,16 @@ public class Main
         ProcessBuilder processBuilder = new ProcessBuilder();
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
-        } 
-        return 4567;  
+        }
+        return 4567;
     }
 
     static double calculateListPrice(HashMap<Product, Integer> items) {
         double total = 0;
-
 
         for (Product key : items.keySet()) {
             total += key.getPrice() * items.get(key);
         }
         return total;
     }
-
-
-
-
 }
--- rest-api/src/main/java/com/acme/dataobjects/ItemResponse.java
+++ rest-api/src/main/java/com/acme/dataobjects/ItemResponse.java
@@ -7,20 +7,20 @@ public class ItemResponse {
     private final ArrayList<OutputProduct> items;
     private double total;
 
-    public ItemResponse(HashMap<Product, Integer> inputItems, HashMap<DiscountBundle, Integer> inputDiscount){
-        
-        
+    public ItemResponse(HashMap<Product, Integer> inputItems, HashMap<DiscountBundle, Integer> inputDiscount) {
 
         this.items = new ArrayList<OutputProduct>();
 
-        
         for (Product product : inputItems.keySet()) {
-            items.add(new OutputProduct(product.getName(), product.getPrice() * inputItems.get(product), inputItems.get(product)));
+            items.add(new OutputProduct(product.getName(), product.getPrice() * inputItems.get(product),
+                    inputItems.get(product)));
         }
 
-        for (DiscountBundle discountBundle : inputDiscount.keySet()) {
-            items.add(new OutputProduct(discountBundle.getName(), discountBundle.getPrice() * inputItems.get(discountBundle), inputItems.get(discountBundle)));
-        }
+        // for (DiscountBundle discountBundle : inputDiscount.keySet()) {
+        // items.add(new OutputProduct(discountBundle.getName(),
+        // discountBundle.getPrice() * inputItems.get(discountBundle),
+        // inputItems.get(discountBundle)));
+        // }
 
         this.total = 0;
         for (OutputProduct product : items) {
@@ -28,12 +28,12 @@ public class ItemResponse {
         }
     }
 
-       public ItemResponse(HashMap<Product, Integer> inputItems){
+    public ItemResponse(HashMap<Product, Integer> inputItems) {
         this.items = new ArrayList<OutputProduct>();
 
-        
         for (Product product : inputItems.keySet()) {
-            items.add(new OutputProduct(product.getName(), product.getPrice() * inputItems.get(product), inputItems.get(product)));
+            items.add(new OutputProduct(product.getName(), product.getPrice() * inputItems.get(product),
+                    inputItems.get(product)));
         }
 
         this.total = 0;
@@ -42,7 +42,3 @@ public class ItemResponse {
         }
     }
 }
-
-
-
-
